2249bac2b967d9aac2c9b0b8d1bf7c08c0cb3dae
Grant <grant.achuzia@gmail.com>
2026-03-09 21:21:26 -0400

    feat: add arg parser for cli

--- twit/cli.py
+++ twit/cli.py
@@ -1,2 +1,24 @@
+import argparse
+
+def parse_args():
+    """
+    Implements sub commands using the built-in Python library
+    """
+    parser = argparse.ArgumentParser()
+    
+    commands = parser.add_subparsers(dest='comman')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+def init(args):
+    """
+    """
+    print("Hello, World!") #TODO: make this an actual init command
+
 def main():
-    print("Hellow, World!")
+    args = parse_args()
+    args.func(args)
