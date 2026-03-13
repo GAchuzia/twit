83086065d9cacc9c8fcbedf5be42e6fa323a77f8
Grant Achuzia <grant.achuzia@gmail.com>
2026-03-13 02:40:52 -0400

    feat: update subcommands with the hash-object command

--- twit/cli.py
+++ twit/cli.py
@@ -8,11 +8,15 @@ def parse_args():
     """
     parser = argparse.ArgumentParser()
     
-    commands = parser.add_subparsers(dest='comman')
+    commands = parser.add_subparsers(dest='command')
     commands.required = True
 
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
+
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
     return parser.parse_args()
 
@@ -22,6 +26,13 @@ def init(args):
     data.init()
     print(f'Initialized empty twit repository in {os.getcwd()}/{data.GIT_DIR}')
 
+def hash_object(args):
+    """
+    
+    """
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+        
 def main():
     args = parse_args()
     args.func(args)
