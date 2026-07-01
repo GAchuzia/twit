e9cb866c1137d6446c89e0de8fb528602b26847a
Grant Achuzia <grant.achuzia@gmail.com>
2026-07-01 17:38:48 -0400

    feat: add logic to ignore .twit files in the write-tree command

--- twit/base.py
+++ twit/base.py
@@ -8,7 +8,12 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 print(full)
-            elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+
+    # TODO: Create tree object 
+
+def is_ignored(path):
+    return '.twit' in path.split('/')
